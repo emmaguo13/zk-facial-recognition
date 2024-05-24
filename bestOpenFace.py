ce90e9bbbf07f8bdc41ed3542cc5ef2741f2edbe
emmaguo13 <emmaguo@berkeley.edu>
2024-05-24 16:40:39 +0900

    :)

--- bestOpenFace.py
+++ bestOpenFace.py
@@ -448,16 +448,16 @@ if __name__ == '__main__':
     I_ = torch.cat(imgs, 0)
     I_ = Variable(I_, requires_grad=False)
 
-    # torch.onnx.export(nof,               # model being run
-    #                   I_,                   # model input (or a tuple for multiple inputs)
-    #                   "openface_pt_native.onnx",            # where to save the model (can be a file or file-like object)
-    #                   export_params=True,        # store the trained parameter weights inside the model file
-    #                   opset_version=10,          # the ONNX version to export the model to
-    #                   do_constant_folding=True,  # whether to execute constant folding for optimization
-    #                   input_names = ['input'],   # the model's input names
-    #                   output_names = ['output'], # the model's output names
-    #                   dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
-    #                                 'output' : {0 : 'batch_size'}})
+    torch.onnx.export(nof,               # model being run
+                      I_,                   # model input (or a tuple for multiple inputs)
+                      "openface_pt_native_best.onnx",            # where to save the model (can be a file or file-like object)
+                      export_params=True,        # store the trained parameter weights inside the model file
+                      opset_version=10,          # the ONNX version to export the model to
+                      do_constant_folding=True,  # whether to execute constant folding for optimization
+                      input_names = ['input'],   # the model's input names
+                      output_names = ['output'], # the model's output names
+                      dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
+                                    'output' : {0 : 'batch_size'}})
 
     start = time.time()
     cos_sim, f, f_736 = nof(I_)
